6807fe60600a7047521167ddbad14fbdfe6a66d5
Vineet Singh Yadav <Vineetsingh0313@gmail.com>
2025-07-20 17:02:59 +0530

    Tic Tac toe

---
+++ script.js
@@ -0,0 +1,69 @@
+var turn = "X";
+var gameOver = false;
+
+function changeTurn (){
+    return turn === "X"?"O" : "X"
+}
+
+function playSound() {
+  let audio = new Audio('audio/turn.mp3'); 
+  audio.play();
+}
+
+function winSound() {
+  let audio = new Audio('audio/yay.mp3'); 
+  audio.play();
+}
+
+
+
+var boxs = document.querySelectorAll(".gridBox");
+function cheakWin() {
+
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    wins.forEach(e => {
+        if((boxs[e[0]].innerHTML === boxs[e[1]].innerHTML) && (boxs[e[0]].innerHTML === boxs[e[2]].innerHTML) && (boxs[e[0]].innerHTML !== "")){
+            document.getElementsByClassName('Player')[0].innerHTML = boxs[e[0]].innerHTML + " Won";
+            gameOver = true;
+            winSound();
+        }
+    });
+}
+
+
+var length = document.querySelectorAll(".gridBox").length;
+for(i=0;i < length;i++){
+ document.querySelectorAll(".gridBox")[i].addEventListener('click',function(){
+    if (gameOver) return;
+    var inbox = this.innerHTML
+    if(inbox === ''){
+        this.innerHTML = turn;
+        turn = changeTurn();
+        playSound();
+        cheakWin();
+        if(!gameOver){document.getElementsByClassName('Player')[0].innerHTML = "Turn For " + turn;}
+    }
+}
+ );   
+}
+
+document.getElementById("button").addEventListener("click", function () {
+     boxs.forEach(box => {
+        box.innerHTML = "";
+        box.style.pointerEvents = "auto";//enable clicks again
+     });
+     turn = "X";
+     document.getElementsByClassName('Player')[0].innerHTML = "Turn For X";
+     gameOver = false;
+
+});
+ 
